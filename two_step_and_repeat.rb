fac43d4ea3ba6089896a57f4c4b91dedf520d6c2
maryjane spangler <maryjanespangler13@gmail.com>
2018-08-07 21:34:07 +0000

    automatically backed up by learn

--- two_step_and_repeat.rb
+++ two_step_and_repeat.rb
@@ -13,4 +13,26 @@ def first_steps
 
 def a_few_more_steps
   loop do 
-    
+   puts "right foot back"
+   sleep (0.5)
+   puts "left foot back"
+   sleep (0.5)
+   puts "right foot back"
+  puts "stop"
+  sleep (1) 
+  puts "Right foot steps right and back"
+    sleep (0.5)
+puts "Left foot crosses over right"
+    sleep (0.5)
+puts "Right foot steps right"
+    sleep (0.5)
+    puts "stop"
+    sleep (0.5)
+    puts "turn"
+    sleep (1)
+  end 
+end 
+
+def how_many_steps?
+  steps = 0
+  
